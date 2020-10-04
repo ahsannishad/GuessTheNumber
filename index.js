9d5b201abf358bb13fadf605a0c64d51bcd441a4
Ahsan Nishad <ahsannishad@gmail.com>
2020-10-05 00:59:40 +0600

    bug fix

--- index.js
+++ index.js
@@ -1,2 +1,82 @@
-var guessedNumber = document.getElementById(userInput).Value;
-console.log(guessedNumber);
+let number = Math.floor(Math.random() * 10) + 1;
+let usersGuess;
+let attampt = 1;
+let notify = document.getElementById("notify");
+let points = 0;
+let gameOver = false;
+
+
+
+document.getElementById("btn").addEventListener("click", function () {
+
+    usersGuess = document.getElementById("userInput").value;
+    checkLogic();
+    document.getElementById("points").innerHTML = points;
+
+
+
+
+
+
+
+
+
+});
+
+
+//check logic
+function checkLogic() {
+
+
+    if (usersGuess > number) {
+        attampt++;
+        notify.innerHTML = "The number is lower than you thought";
+        setTimeout(function () {
+            notify.innerHTML = "";
+        }, 3000)
+
+
+
+
+    } else if (usersGuess < number) {
+        attampt++;
+        notify.innerHTML = "The number is higher than you thought";
+        setTimeout(function () {
+            notify.innerHTML = "";
+
+        }, 3000)
+
+
+
+
+    } else {
+        alert("Congratulation you have guessed the number correct in " + attampt + " attampt ");
+        if (confirm("Do you want to Play Again?")) {
+            checkPoints();
+            playAgain();
+        }
+    }
+
+}
+
+//check points
+
+function checkPoints() {
+    if (attampt == 1) {
+        points = points + 100;
+    } else if (attampt == 2) {
+        points = points + 50;
+    } else if (attampt == 3) {
+        points = points + 25;
+    } else {
+        points = points + 0;
+    }
+
+}
+
+//play again
+function playAgain() {
+    number = Math.floor(Math.random() * 10) + 1;
+    attampt = 1;
+
+}
